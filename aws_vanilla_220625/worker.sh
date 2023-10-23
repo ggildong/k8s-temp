14bba7d50076704fd6b5a30ba46885d033a52a4a
JCJ <63003165+ggildong@users.noreply.github.com>
2023-10-23 15:52:08 +0900

    Update worker.sh

--- aws_vanilla_220625/worker.sh
+++ aws_vanilla_220625/worker.sh
@@ -3,7 +3,7 @@
 echo ">>>> K8S Node config Start <<<<"
 
 echo "[TASK 1] K8S Controlplane Join - API Server 192.168.10.10" 
-kubeadm join --kubernetes-version=$KUBERNETES_VERSION --token 123456.1234567890123456 --discovery-token-unsafe-skip-ca-verification 192.168.10.10:6443
+kubeadm join --token 123456.1234567890123456 --discovery-token-unsafe-skip-ca-verification 192.168.10.10:6443
 
 echo "[TASK 2] Config kubeconfig" 
 mkdir -p /root/.kube
