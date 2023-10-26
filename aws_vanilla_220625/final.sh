3dccadcccc42adc37fd80f45c510ed789e41666b
JCJ <63003165+ggildong@users.noreply.github.com>
2023-10-26 10:05:13 +0900

    Update final.sh

--- aws_vanilla_220625/final.sh
+++ aws_vanilla_220625/final.sh
@@ -2,7 +2,7 @@
 echo ">>>> K8S Final config Start <<<<"
 
 echo "[TASK 11] Setting PS1"
-kubectl config rename-context "kubernetes-admin@kubernetes" "HomeLab"
+kubectl config rename-context "kubernetes-admin@kubernetes" "k8sdemo"
 
 echo "[TASK 12] Dynamically provisioning persistent local storage with Kubernetes on k8s-m node - v0.0.22"
 kubectl apply -f https://raw.githubusercontent.com/ggildong/k8s-temp/main/aws_vanilla_220625/local-path-storage.yaml
@@ -18,22 +18,5 @@ printf 'tolerations: [{key: node-role.kubernetes.io/master, operator: Exists, ef
 echo "[TASK 14] K8S v1.24? : k8s-m node config taint & label"
 kubectl taint node k8s-m node-role.kubernetes.io/control-plane- >/dev/null 2>&1
 kubectl label nodes k8s-m node-role.kubernetes.io/master= >/dev/null 2>&1
-
-echo "Setting MetalLB"
-
-echo "[TASK 15] Change proxy-mode=ipvs"
-echo "# see what changes would be made, returns nonzero returncode if different"
-kubectl get configmap kube-proxy -n kube-system -o yaml | \
-sed -e "s/strictARP: false/strictARP: true/" | \
-sed -e "s/mode: .*/mode: \"ipvs\"/" | \
-kubectl diff -f - -n kube-system
-
-echo "# actually apply the changes, returns nonzero returncode on errors only"
-kubectl get configmap kube-proxy -n kube-system -o yaml | \
-sed -e "s/strictARP: false/strictARP: true/" | \
-sed -e "s/mode: .*/mode: \"ipvs\"/" | \
-kubectl apply -f - -n kube-system
-
-kubectl rollout restart ds kube-proxy -n kube-system
 
 echo ">>>> K8S Final Config End <<<<"
