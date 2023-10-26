663508989a66d7368fdb1bc9a6b81124917c3314
JCJ <63003165+ggildong@users.noreply.github.com>
2023-10-26 10:11:37 +0900

    Update final.sh

--- aws_vanilla_220625/final.sh
+++ aws_vanilla_220625/final.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 echo ">>>> K8S Final config Start <<<<"
 
-echo "[TASK 11] Setting PS1"
+echo "[TASK 11] rename-context"
 kubectl config rename-context "kubernetes-admin@kubernetes" "k8sdemo"
 
 echo "[TASK 12] Dynamically provisioning persistent local storage with Kubernetes on k8s-m node - v0.0.22"
@@ -15,7 +15,7 @@ printf 'tolerations: [{key: node-role.kubernetes.io/master, operator: Exists, ef
   --set nfs.server=$(cat /root/efs.txt) --set nfs.path=/ --set nodeSelector."kubernetes\.io/hostname"=k8s-m \
   --values /dev/stdin
 
-echo "[TASK 14] K8S v1.24? : k8s-m node config taint & label"
+echo "[TASK 14] k8s-m node config taint & label"
 kubectl taint node k8s-m node-role.kubernetes.io/control-plane- >/dev/null 2>&1
 kubectl label nodes k8s-m node-role.kubernetes.io/master= >/dev/null 2>&1
 
