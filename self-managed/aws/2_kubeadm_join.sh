a8df6df7de6e3380a9fa292f0adefb6b0a2d2745
JCJ <63003165+ggildong@users.noreply.github.com>
2023-11-01 10:23:02 +0900

    Update 2_kubeadm_join.sh

--- self-managed/aws/2_kubeadm_join.sh
+++ self-managed/aws/2_kubeadm_join.sh
@@ -3,6 +3,6 @@
 echo ">>>> kubeadm join Begin <<<<"
 
 echo "[TASK 1] K8S Node Join - API Server 192.168.10.10" 
-kubeadm join --kubernetes-version=$KUBERNETES_VERSION --token 123456.1234567890123456 --discovery-token-unsafe-skip-ca-verification 192.168.10.10:6443
+kubeadm join --token 123456.1234567890123456 --discovery-token-unsafe-skip-ca-verification 192.168.10.10:6443
 
 echo ">>>> kubeadm join End <<<<"
