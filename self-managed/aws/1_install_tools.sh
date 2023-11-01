fc2a41b48d0def1719be12551733660882a9156e
JCJ <63003165+ggildong@users.noreply.github.com>
2023-11-01 09:15:37 +0900

    Add files via upload

---
+++ self-managed/aws/1_install_tools.sh
@@ -0,0 +1,99 @@
+#!/bin/bash -xe
+echo ">>>> Install Tools Begin <<<<"
+echo "[TASK 1] Setting Root Password"
+printf "Pa55W0rd\nPa55W0rd\n" | passwd
+
+echo "[TASK 2] Setting Sshd Config"
+sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+sed -i "s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
+systemctl restart sshd
+# echo  > .ssh/authorized_keys
+
+echo "[TASK 3] Change Timezone & Setting Profile & Bashrc"
+# Change Timezone
+ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+#  Setting Profile & Bashrc
+echo 'alias vi=vim' >> /etc/profile
+echo "sudo su -" >> /home/ubuntu/.bashrc
+
+echo "[TASK 4] Disable ufw & AppArmor"
+systemctl stop ufw && systemctl disable ufw
+systemctl stop apparmor && systemctl disable apparmor
+
+echo "[TASK 5] Install Packages"
+apt update && apt install -y tree jq sshpass bridge-utils net-tools bat exa duf nfs-common sysstat
+# echo "alias cat='batcat --paging=never'" >> /etc/profile
+
+echo "[TASK 6] Setting hostname Using AWS Private DNS"
+hostnamectl set-hostname $(curl -s http://169.254.169.254/latest/meta-data/local-hostname)
+#echo "[TASK 6] Setting Local DNS Using Hosts file"
+#echo "192.168.10.10 k8s-m" >> /etc/hosts
+#echo "192.168.10.101 k8s-w1" >> /etc/hosts
+#echo "192.168.10.102 k8s-w2" >> /etc/hosts
+#echo "192.168.20.103 k8s-w3" >> /etc/hosts
+
+echo "[TASK 7] Install containerd.io"
+echo "swap off"
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+# Install Runtime - Containerd https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+cat <<EOF > /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF > /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sysctl -p 
+sysctl --system 
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
+apt-get update 
+apt-get install containerd.io -y
+mkdir -p /etc/containerd
+containerd config default > /etc/containerd/config.toml
+
+echo "[TASK 8] Using the systemd cgroup driver"
+#sed -i'' -r -e "/runc.options/a\            SystemdCgroup = true" /etc/containerd/config.toml
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+systemctl restart containerd
+
+# Change container runtime args
+# KUBELET_KUBEADM_ARGS=--container-runtime=remote --container-runtime-endpoint=/run/containerd/containerd.sock --cgroup-driver=systemd
+
+cat <<EOF > /etc/default/kubelet
+KUBELET_KUBEADM_ARGS=--container-runtime-endpoint=/run/containerd/containerd.sock --cgroup-driver=systemd
+EOF
+
+# Change runtime endpoint
+cat <<EOF > /etc/crictl.yaml
+runtime-endpoint: unix:///run/containerd/containerd.sock
+image-endpoint: unix:///run/containerd/containerd.sock
+EOF
+
+echo "[TASK 9] Install Kubernetes components (kubeadm, kubelet and kubectl)"
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl
+
+# curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# curl -fsSL https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+# echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v'$KUBERNETES_VERSION_SHORT'/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+apt-get update && apt-get install -y kubelet=$KUBERNETES_VERSION-00 kubectl=$KUBERNETES_VERSION-00 kubeadm=$KUBERNETES_VERSION-00
+apt-mark hold kubelet kubeadm kubectl
+
+systemctl enable kubelet && systemctl start kubelet
+
+echo ">>>> Install Tools End <<<<"
---
+++ self-managed/aws/2_kubeadm_init.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -xe
+echo ">>>> kubeadm init Begin <<<<"
+
+echo "get my public ipv4"
+PUBLICIP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+
+echo "[TASK 1] Initial Kubernetes - Pod CIDR 172.16.0.0/16 , Service CIDR 10.200.1.0/24 , API Server 192.168.10.10"
+
+if  echo "$PUBLICIP" | grep -E '(^[0-2][0-5]{1,2}?\.|^[3-9][0-9]?\.)([0-2][0-5]{1,2}?\.|[3-9][0-9]?\.)([0-2][0-5]{1,2}?\.|[3-9][0-9]?\.)([0-2][0-5]{1,2}?$|[3-9][0-9]?$)' >/dev/null; then
+  echo "public ipv4: true"
+  kubeadm init --kubernetes-version=$KUBERNETES_VERSION --token 123456.1234567890123456 --token-ttl 0 --pod-network-cidr=172.16.0.0/16 --apiserver-advertise-address=192.168.10.10 --service-cidr 10.200.1.0/24 --apiserver-cert-extra-sans $PUBLICIP 
+else
+  echo "public ipv4: false"
+  kubeadm init --kubernetes-version=$KUBERNETES_VERSION --token 123456.1234567890123456 --token-ttl 0 --pod-network-cidr=172.16.0.0/16 --apiserver-advertise-address=192.168.10.10 --service-cidr 10.200.1.0/24 
+fi
+
+echo "[TASK 2] Setting kube config file"
+mkdir -p /root/.kube
+cp -i /etc/kubernetes/admin.conf /root/.kube/config
+chown $(id -u):$(id -g) /root/.kube/config
+
+echo "[TASK 3] Source the completion"
+echo 'source <(kubectl completion bash)' >> /etc/profile
+
+echo "[TASK 4] Alias kubectl to k"
+echo 'alias k=kubectl' >> /etc/profile
+echo 'complete -F __start_kubectl k' >> /etc/profile
+
+# echo "[TASK 5] Install kubectx & kubens"
+# git clone https://github.com/ahmetb/kubectx /opt/kubectx
+# ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+# ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+
+# echo "[TASK 6] Install kubeps"
+# git clone https://github.com/jonmosco/kube-ps1.git /root/kube-ps1
+# cat <<"EOT" >> /root/.bash_profile
+# source /root/kube-ps1/kube-ps1.sh
+# KUBE_PS1_SYMBOL_ENABLE=true
+# KUBE_PS1_SYMBOL_DEFAULT=🐤
+# function get_cluster_short() {
+#   echo "$1" | cut -d . -f1
+# }
+# KUBE_PS1_CLUSTER_FUNCTION=get_cluster_short
+# KUBE_PS1_SUFFIX=') '
+# PS1='$(kube_ps1)'$PS1
+# EOT
+
+echo "[TASK 7] Install kubetail & etcd"
+apt install kubetail etcd-client -y
+
+echo "[TASK 8] Install helm"
+curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+echo "[TASK 9] Create Directory (nfs4-share)"
+mkdir /nfs4-share
+
+echo ">>>> kubeadm init End <<<<"
